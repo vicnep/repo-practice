ddafaa78b6eb310aa0c05a0dbb4dc05ee75712f5
Victor Perez Ramos <talychos@gmail.com>
2025-08-24 20:13:24 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,8 +1,9 @@
-nombre = "Naomi Camila"
+nombre = "Naomi Camilita"
 apellido = "Ann"
 
 print("Hola " + nombre + " " + apellido + " ¿cómo estás?")
 saludo = ("Hola " + nombre + " " + apellido)
 print(saludo)
+
 
 
